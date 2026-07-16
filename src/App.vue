5685419da87a9ce34342cf2fd467a5e8b1f27c93
dannysir <dannysir@naver.com>
2026-07-16 10:48:57 +0900

    feat: 답변 링크 UI 제작 및 연결

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,13 @@ import TheHeader from '@/components/layouts/Header.vue'
 import TheFooter from '@/components/layouts/Footer.vue'
 import ChatbotWidget from '@/components/chat/ChatBot.vue'
 import { RouterView } from 'vue-router'
-
 </script>
 
 <template>
   <div class="app-layout">
     <TheHeader />
     <main class="app-main">
-      <RouterView />
+      <RouterView :key="$route.fullPath" />
     </main>
     <TheFooter />
     <ChatbotWidget />
